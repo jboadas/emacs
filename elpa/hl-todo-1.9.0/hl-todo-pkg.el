ffe64de490acc850d741c33c731db6e4587dbb99
jboadas <jboadas@gmail.com>
2018-08-24 09:32:54 -0400

    upgrade packages

---
+++ elpa/hl-todo-1.9.0/hl-todo-pkg.el
@@ -0,0 +1,1 @@
+(define-package "hl-todo" "1.9.0" "highlight TODO and similar keywords" 'nil :commit "770c9862ed79a2437b764050a1006c62f9e32747" :keywords '("convenience") :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/tarsius/hl-todo")
